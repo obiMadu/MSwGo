63708e54c2b8947a6059e2ef72cdbe369ba4464c
Obi.M <mail@obimadu.pro>
2024-06-15 21:47:45 +0100

    feat(auth): bootstrap db connection

--- authentication/cmd/api/main.go
+++ authentication/cmd/api/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
+	"time"
+
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 const webPort string = ":80"
+
+var counts int
 
 type Config struct{}
 
@@ -14,6 +21,14 @@ func main() {
 	app := Config{}
 
 	// define http server
+
+	// connect to the db
+	db := connectToDB()
+	if db == nil {
+		log.Panic("Can't connect to Postgress")
+	}
+
+	// create http server
 	router := gin.Default()
 
 	app.routes(router)
@@ -21,4 +36,49 @@ func main() {
 	// start the server
 	log.Printf("Starting auth service on port %s\n", webPort)
 	router.Run(webPort)
+}
+
+func openDB(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	// return *sql.DB from db(*gorm.DB) to enable Ping()
+	gormDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	// ping database
+	err = gormDB.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *gorm.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready ...")
+			counts++
+		} else {
+			log.Println("Connected to Postgres!")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for three seconds....")
+		time.Sleep(3 * time.Second)
+		continue
+	}
 }
