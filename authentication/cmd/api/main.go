a7858df97961ce24c9c027de029aebd7110ae35c
Obi.M <mail@obimadu.pro>
2024-06-15 20:46:22 +0100

    feat(auth): bootstrap auth http server

--- authentication/cmd/api/main.go
+++ authentication/cmd/api/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+const webPort string = ":80"
+
+type Config struct{}
+
 func main() {
-	//
+	app := Config{}
+
+	// define http server
+	router := gin.Default()
+
+	app.routes(router)
+
+	// start the server
+	log.Printf("Starting auth service on port %s\n", webPort)
+	router.Run(webPort)
 }
---
+++ authentication/cmd/api/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func (app *Config) routes(r *gin.Engine) {
+
+	// setup cross-origin resourse sharing middleware
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposeHeaders:    []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	// routes
+
+}
