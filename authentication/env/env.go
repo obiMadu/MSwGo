21d8c21231a659e4efe1e787a865e41726682729
Obi.M <mail@obimadu.pro>
2024-06-15 21:50:01 +0100

    feat(auth): create env package

---
+++ authentication/env/env.go
@@ -0,0 +1,14 @@
+package env
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Failed to load .env file")
+	}
+}
